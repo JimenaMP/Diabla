2f34e3f803c3e97e4e257545183060164f1ae15d
JimenaMP <jimenarnbww@gmail.com>
2018-02-08 17:48:11 -0600

    Cambio d color del botonnnn

--- app/src/main/java/diabla/org/diabla/MainActivity.java
+++ app/src/main/java/diabla/org/diabla/MainActivity.java
@@ -2,6 +2,8 @@ package diabla.org.diabla;
 
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
 
 
         //hgfghfghfgfghfgfghfghfghfg
+        findViewById(R.id.enviar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Aqui va lo que se va a ver cuando se de click el el boton
+                //toast :D
+                Toast.makeText(getApplicationContext(), "TU MENSAJE SE ENVIA",Toast.LENGTH_LONG).show();
+            }
+        });
+        //Ya tenemos el boton, ya podemos dar ordenes
+        //Set es la orden. es una clase interna anonima, dentro de los parentesis va
+        //el evebto del boton....
     }
 }
